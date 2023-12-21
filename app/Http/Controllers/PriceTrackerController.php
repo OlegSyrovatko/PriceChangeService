66f89e6af82063c4b8af9a7a61e9197fc1d1d91d
Oleg <sirov@ukr.net>
2023-12-21 21:08:15 +0200

    add controller

---
+++ app/Http/Controllers/PriceTrackerController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\PriceTracker;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Mail;
+
+class PriceTrackerController extends Controller
+{
+    public function subscribe(Request $request)
+    {
+        $request->validate([
+            'url' => 'required|url',
+            'email' => 'required|email',
+        ]);
+
+        // Отримання ціни з оголошення на OLX (потрібно налаштувати відповідно до структури сайту)
+        $response = Http::get($request->input('url'));
+        $currentPrice = $this->extractPriceFromResponse($response->body());
+
+        // Збереження даних у базі даних
+        $tracker = PriceTracker::create([
+            'url' => $request->input('url'),
+            'current_price' => $currentPrice,
+            'email' => $request->input('email'),
+        ]);
+
+        // Відправлення листа підписникові (потрібно налаштувати відповідно до вашої логіки)
+        $this->sendEmailNotification($tracker);
+
+        return response()->json(['message' => 'Subscription successful']);
+    }
+
+    protected function extractPriceFromResponse($html)
+    {
+        // Логіка для вилучення ціни з HTML-коду оголошення на OLX
+        // Наприклад, використовуйте бібліотеку для парсингу HTML, таку як Symfony DomCrawler
+        // Простий приклад: $crawler = new Crawler($html); $price = $crawler->filter('.price')->text();
+        // Налаштуйте залежно від структури OLX
+        return 0.0; // Замініть на реальний код
+    }
+
+    protected function sendEmailNotification(PriceTracker $tracker)
+    {
+        // Логіка для відправлення листа (використовуйте Laravel Mail)
+        // Наприклад, Mail::to($tracker->email)->send(new PriceChangeNotification($tracker));
+        // Де PriceChangeNotification - клас для листа, який ви створите вручну
+        // Налаштуйте залежно від вашої логіки
+    }
+}
